5dbc3187ac84ea9157416d8f91d30b0444f16360
Gabriel Aureo <gabrielaureo@gmail.com>
2019-09-26 15:44:22 -0300

    doasdokas

--- Assets/VideoCartao2.cs
+++ Assets/VideoCartao2.cs
@@ -1,6 +1,48 @@
 using UnityEngine;
 using UnityEngine.Video;
+using DG.Tweening;
+using Vuforia;
 
-public class VideoCartao2 : MonoBehaviour{
-    VideoPlayer videoPlayer;
+public class VideoCartao2 : MonoBehaviour, ITrackableEventHandler{
+    public VideoPlayer videoPlayer;
+    public ParticleSystem m_particleSystem;
+    public Transform plane;
+    private Material material;
+
+    void Awake(){
+        material = plane.GetComponent<MeshRenderer>().material;
+        //FadeIn();
+        videoPlayer.loopPointReached += PlayParticles;
+        var m_TrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (m_TrackableBehaviour) {
+            m_TrackableBehaviour.RegisterTrackableEventHandler(this);
+
+        }
+    }
+
+    private void PlayParticles(VideoPlayer vp){
+        material.DOFade(0, .5f);
+        m_particleSystem.Play();
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {            
+            FadeIn();
+            
+        }
+        else
+        {
+        }
+    }
+
+    void FadeIn(){
+        videoPlayer.time = 0;
+        var color =  material.GetColor("_Color");
+        material.SetColor("_Color", new Color(color.r, color.g, color.b, 1));
+        material.DOFade(1f, .5f);
+    }
 }
